607a77c4518e9e319130cd33ccb07d910a8fb343
diego murillo <diego.spd@gmail.com>
2018-08-20 15:03:57 -0500

    Add binaries

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PATH=$PATH:~/.dotfiles/bin
+export PATH=$PATH:~/.local/bin/:/~/.cabal/bin
+export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
 
 export EDITOR=nano
 export VBOX_USB=usbfs
@@ -9,7 +12,3 @@ export SVN_EDITOR=nano
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk/
 export GEOSERVER_HOME=~/storage/codigos/mapitas/geoserver-2.8.3
 export HADOOP_HOME=~/storage/codigos/hadoop
-
-
-export PATH=$PATH:~/.local/bin/:/~/.cabal/bin
-export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
