22ba483702eee76baf52df13b3440bd347dd54a8
diego murillo <diego.spd@gmail.com>
2018-09-28 15:03:51 -0500

    Remove oh-my-zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo pacman -S zsh ruby --needed
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -s ~/.dotfiles/zsh/,zshrc ~/.zshrc
 chsh -s /bin/zsh
