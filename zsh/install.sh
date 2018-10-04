d9dbda74d2c3508e428df16a0394d76ba5b9d629
diego murillo <diego.spd@gmail.com>
2018-10-04 10:44:40 -0500

    Better install

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,5 +2,9 @@
 
 sudo pacman -S zsh ruby --needed
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+if [ -f  ~/.zshrc ]; then
+	rm ~/.zshrc
+fi
 ln -s ~/.dotfiles/zsh/,zshrc ~/.zshrc
 chsh -s /bin/zsh
