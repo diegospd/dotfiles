a7a421e3cfe86be4ac20aec633375a6aa8ecf347
diego murillo <diego.spd@gmail.com>
2018-10-21 16:59:08 -0500

    Use xmonad instead of kwin

--- xmonad/enable_xmonad.sh
+++ xmonad/enable_xmonad.sh
@@ -3,5 +3,7 @@
 mkdir -p ~/.config/plasma-workspace/env/
 # echo "KDEWM=/usr/bin/stack --system-ghc exec xmonad" > ~/.config/plasma-workspace/env/set_window_manager.sh
 
-echo "KDEWM=~/.xmonad/xmonad-x86_64-linux" > ~/.config/plasma-workspace/env/set_window_manager.sh
+echo "KDEWM=$HOME/.local/bin/xmonad" > ~/.config/plasma-workspace/env/set_window_manager.sh
+# echo "KDEWM=~/.local/bin/xmonad" > ~/.config/plasma-workspace/env/set_window_manager.sh
+
 chmod +x ~/.config/plasma-workspace/env/set_window_manager.sh
