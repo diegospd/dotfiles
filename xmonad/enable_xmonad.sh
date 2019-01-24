51bdbfe2cb1890eed65a42ca331b81dfd8b3e7b1
Unknown <diego.spd@gmail.com>
2019-01-24 13:48:01 -0600

    change binary name

--- xmonad/enable_xmonad.sh
+++ xmonad/enable_xmonad.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ## sets xmonad as the window manager for plasma
 mkdir -p ~/.config/plasma-workspace/env/
-echo "KDEWM=$HOME/.local/bin/xmonad-x86_64-linux" > ~/.config/plasma-workspace/env/set_window_manager.sh
+echo "KDEWM=$HOME/.local/bin/my-xmonad" > ~/.config/plasma-workspace/env/set_window_manager.sh
 chmod +x ~/.config/plasma-workspace/env/set_window_manager.sh
