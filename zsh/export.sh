d6f78ff4f61538100738ea98f9f32c07c045bb42
Diego Murillo <diego.albarran@nubank.com.br>
2019-05-05 15:09:08 -0300

    nu cli

--- zsh/export.sh
+++ zsh/export.sh
@@ -8,6 +8,9 @@ export EDITOR=nano
 export VBOX_USB=usbfs
 export SVN_EDITOR=nano
 
+export NU_HOME="${HOME}/dev/nu"  # folder where repos should be cloned; it's an arbitrary name, so change it if you wish
+export NUCLI_HOME="${NU_HOME}/nucli"
+export PATH="${NUCLI_HOME}:${PATH}"
 
 # export JAVA_HOME=/usr/lib/jvm/java-7-openjdk
 # export GEOSERVER_HOME=~/storage/codigos/mapitas/geoserver-2.8.3
@@ -15,3 +18,4 @@ export SVN_EDITOR=nano
 
 # You may need to manually set your language environment
 # export LANG=en_US.UTF-8
+export GPG_TTY=$(tty)
