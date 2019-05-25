71655f5aecf102f197e3240c5a30a73f78e68f37
Unknown <diego.spd@gmail.com>
2019-05-25 17:05:21 -0500

    bins

---
+++ bin/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln -s $HOME/.dotfiles/pacman/install_root.sh $HOME/.dotfiles/bin/bin/archUpdate
--- zsh/utils.sh
+++ zsh/utils.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 
 alias ..='cd ..'
@@ -38,40 +38,7 @@ orphans() {
 }
 
 
-extract() {
-    local c e i
 
-    (($#)) || return
-
-    for i; do
-        c=''
-        e=1
-
-        if [[ ! -r $i ]]; then
-            echo "$0: file is unreadable: \`$i'" >&2
-            continue
-        fi
-
-        case $i in
-            *.t@(gz|lz|xz|b@(2|z?(2))|a@(z|r?(.@(Z|bz?(2)|gz|lzma|xz)))))
-                   c=(bsdtar xvf);;
-            *.7z)  c=(7z x);;
-            *.Z)   c=(uncompress);;
-            *.bz2) c=(bunzip2);;
-            *.exe) c=(cabextract);;
-            *.gz)  c=(gunzip);;
-            *.rar) c=(unrar x);;
-            *.xz)  c=(unxz);;
-            *.zip) c=(unzip);;
-            *)     echo "$0: unrecognized file extension: \`$i'" >&2
-                   continue;;
-        esac
-
-        command "${c[@]}" "$i"
-        ((e = e || $?))
-    done
-    return "$e"
-}
 
 
 # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux#5947802
