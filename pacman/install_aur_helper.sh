559098d9f0b7d7b4aaa3e3c89c434b7163742505
diego murillo <diego.albarran@nubank.com.br>
2020-03-03 21:19:22 -0600

    yay

--- pacman/install_aur_helper.sh
+++ pacman/install_aur_helper.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ###  AUR support via yay
+## https://aur.archlinux.org/packages/yay/
 ## https://github.com/Jguer/yay
 git clone https://github.com/Jguer/yay.git
+cp yay.PKGBUILD yay/PKGBUILD
 cd yay
 makepkg -si
 cd ..
